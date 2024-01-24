be8e70329acf88ea1e048d00b2870013123ebd9d
PCuser <hitkillerman@gmail.com>
2024-01-24 23:02:17 +0300

    1comm

---
+++ timeclient.py
@@ -0,0 +1,10 @@
+import socket
+
+HOST = input("Введите IP-адрес сервера: ")
+PORT = 1303
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    data = s.recv(1024)
+
+print('Полученное время:', data.decode())
